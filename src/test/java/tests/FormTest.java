6e7836c001e6fbf143a2bcd00bdadaafa2959500
Alexia910 <lesya.91099@gmail.com>
2022-02-09 22:40:18 +0300

    Updated FormTest

--- src/test/java/tests/FormTest.java
+++ src/test/java/tests/FormTest.java
@@ -3,7 +3,6 @@ package junit;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
